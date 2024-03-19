2e6d7741ca4eebc0ca1a5785a05cfe6c30e257e6
afanjohn <johnaffann@gmail.com>
2024-03-19 12:05:21 +0800

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Exercise2_05DDT21F2058"
+include(":app")
